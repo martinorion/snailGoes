20b8eb376bda0770f5eac0dd8027c199a8f26dc2
MartinHolena <martinkiko18@gmail.com>
2021-09-11 11:12:15 +0200

    totalDistance

--- src/Snail.java
+++ src/Snail.java
@@ -1,5 +1,12 @@
 public class Snail {
     public static void main(String[] args){
-        System.out.println("Hello Bro");
+        System.out.println("Hello Bro!");
+        totalDistance(0.2,0.4, 100);
     }
+
+    static void totalDistance(double heightStair, double lengthStair, double total){
+
+        System.out.println((total / heightStair) *(lengthStair + heightStair ));
+    }
+
 }
